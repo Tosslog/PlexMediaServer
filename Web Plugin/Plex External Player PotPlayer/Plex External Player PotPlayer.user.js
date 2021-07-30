e8633b6caaab22b266112942604c03bf32ffbace
Tosslog-admin <47279146+Tosslog-admin@users.noreply.github.com>
2021-07-30 16:19:05 +0800

    Update Plex External Player PotPlayer.user.js

--- Web Plugin/Plex External Player PotPlayer/Plex External Player PotPlayer.user.js
+++ Web Plugin/Plex External Player PotPlayer/Plex External Player PotPlayer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Plex External Player PotPlayer
 // @namespace    https://github.com/Tosslog/PlexMediaServer/tree/main/Web%20Plugin/Plex%20External%20Player%20PotPlayer
-// @version      1.1.0
+// @version      1.2.0
 // @description  插件用于激活本地PotPlayer 播放器使用。
 // @author       北京土著 30344386@qq.com
 // @include     /^https?://.*:32400/web.*
@@ -21,7 +21,7 @@ $("head").append(
 // 消息设定
 toastr.options = {
     "closeButton": true,
-    "debug": false,
+    "debug": true,
     "newestOnTop": true,
     "progressBar": true,
     "positionClass": "toast-bottom-right",
@@ -42,7 +42,7 @@ var showToast = function (msg, error) {
     var title = 'Plex External PotPlayer';
     if (error) {
         toastr.error(msg, title, { timeOut: 10000 });
-        logMessage(msg, 'Error');
+        MSG(msg, 'Error');
     }
     else {
         toastr.success(msg, title);
@@ -50,8 +50,54 @@ var showToast = function (msg, error) {
 };
 
 // 控制台输出
-var logMessage = function (msg, _type) {
-    console.log('[Plex External PotPlayer ' + _type + ' ] ' + msg);
+var MSG = function (msg, _type, _tag) {
+    let did = debugID()
+    if (_tag === undefined) {
+        _tag = '[' + did + ']'
+    }
+    else {
+        _tag = '[' + _tag + '-' + did + ']'
+    }
+    _type = '' + _type
+    _type = _type.toLowerCase()
+    if (_type === 'info') {
+        _type = 'INFO'
+
+        let style = 'color: #61afef'
+        console.log('%c ' + _tag + '-[Plex External PotPlayer ' + _type + ']: Bgin ↓', style);
+        console.log(msg);
+        console.log('%c ' + _tag + '-[Plex External PotPlayer ' + _type + ']: End ↑', style);
+    }
+    else if (_type === 'debug') {
+        if (toastr.options['debug'] === true) {
+            _type = 'DEBUG'
+            let style = 'color: #e5c07b'
+            console.log('%c ' + _tag + '-[Plex External PotPlayer ' + _type + ']: Bgin ↓', style);
+            console.log(msg);
+            console.log('%c ' + _tag + '-[Plex External PotPlayer ' + _type + ']: End ↑', style);
+        }
+    }
+    else if (_type === 'warn') {
+        _type = 'WARN'
+        let style = 'color: #e5c07b'
+        console.log('%c ' + _tag + '-[Plex External PotPlayer ' + _type + ']: Bgin ↓', style);
+        console.log(msg);
+        console.log('%c ' + _tag + '-[Plex External PotPlayer ' + _type + ']: End ↑', style);
+    }
+    else if (_type === 'error') {
+        _type = 'ERROR'
+        let style = 'color: #e06c75'
+        console.log('%c ' + _tag + '-[Plex External PotPlayer ' + _type + ']: Bgin ↓', style);
+        console.log(msg);
+        console.log('%c ' + _tag + '-[Plex External PotPlayer ' + _type + ']: End ↑', style);
+    }
+    else {
+        _type = 'LOG'
+        let style = 'color: #98c379'
+        console.log('%c ' + _tag + '-[Plex External PotPlayer ' + _type + ']: Bgin ↓', style);
+        console.log(msg);
+        console.log('%c ' + _tag + '-[Plex External PotPlayer ' + _type + ']: End ↑', style);
+    }
 };
 
 //得到 Plex Servers
@@ -61,9 +107,9 @@ var makeRequest = function (url, serverId) {
         var serverNode = {};
         if (localStorage.users) {
             serverNode = JSON.parse(localStorage.users);
-            //console.log(serverNode);
+            MSG(serverNode, 'debug')
         } else {
-            logMessage('未找到用户详细信息', 'Error');
+            MSG(Language.User_details_not_found, 'error')
         }
         var tokenToTry = origAccessToken;
         if (serverNode === undefined) {
@@ -71,17 +117,17 @@ var makeRequest = function (url, serverId) {
                 users: []
             };
         }
-        // logMessage('寻找 serverId 的令牌:' + serverId)
+
         let tokenFound = false
         if (serverId !== undefined) {
             serverLoop:
             for (var i = 0; i < serverNode.users.length; i++) {
-                // logMessage('Checking server list (' + serverNode.users[i].servers.length + ') for user:' + serverNode.users[i].username)
+                MSG(getJSLocale(Language.Checking_server_list_for_user, { user: serverNode.users[i].servers.length, server: serverNode.users[i].username }))
                 for (var j = 0; j < serverNode.users[i].servers.length; j++) {
-                    // logMessage('Checking server with id ' + serverNode.users[i].servers[j].machineIdentifier)
+                    MSG(getJSLocale(Language.Checking_server_with_id, { id: serverNode.users[i].servers[j].machineIdentifier }), 'debug')
                     if (serverNode.users[i].servers[j].machineIdentifier == serverId) {
                         tokenToTry = serverNode.users[i].servers[j].accessToken;
-                        // logMessage('Token found:' + tokenToTry);
+                        MSG(getJSLocale(Language.Token_found, { token: tokenToTry }), 'debug');
                         tokenFound = true
                         break serverLoop;
                     }
@@ -89,20 +135,20 @@ var makeRequest = function (url, serverId) {
             }
             token = tokenToTry;
             if (!tokenFound) {
-                showToast('找不到身份验证信息', 1);
+                showToast(getJSLocale(Language.No_authentication_information_found), 1);
                 reject();
                 return;
             }
         }
         var authedUrl = url + '&X-Plex-Token=' + tokenToTry;
-        // logMessage('调用 ' + authedUrl, 'Log');
+        MSG(getJSLocale(Language.Verify_permissions_URL, { authurl: authedUrl }), 'debug');
         GM_xmlhttpRequest({
             method: "GET",
 
             url: authedUrl,
             onload: function (state) {
                 if (state.status === 200) {
-                    //logMessage('成功调用' + url, 'Log');
+                    MSG(Language.Verify_permissions_successfully, 'Log');
                     resolve(state);
                 }
             },
@@ -110,10 +156,12 @@ var makeRequest = function (url, serverId) {
                 if (state.readyState === 4) {
 
                     if (state.status === 401) {
-                        logMessage('未授权 ' + url, 'Error');
+                        MSG(Language.unauthorized, 'Error');
+                        showToast(Language.unauthorized, 1)
                     } else if (state.status !== 200) {
-                        logMessage('请求返回 ' + state.status, 'Log');
-                        showToast('Error calling: ' + url + '. Response: ' + state.responseText + ' Code:' + state.status + ' Message: ' + state.statusText, 1);
+                        MSG(getJSLocale(Language.Request_return_status, { status: state.status }), 'error');
+                        MSG(getJSLocale(Language.Call_error_response_code_message, { url: url, responseText: state.responseText, status: state.status, statusText: state.statusText }), 'error')
+                        showToast(getJSLocale(Language.Request_return_status, { status: state.status }), 1);
                     }
                 }
             },
@@ -122,9 +170,9 @@ var makeRequest = function (url, serverId) {
 };
 
 var getHosts = function () {
+    MSG(Language.Find_server_server_address)
     makeRequest('https://plex.tv/api/resources?includeHttps=1&X-Plex-Token=' + localStorage.myPlexAccessToken)
         .then(function (response) {
-            //console.log(response)
             let parts = response.responseXML.getElementsByTagName('Device');
             for (let i = 0; i < parts.length; i++) {
                 if (parts[i].getAttribute('product') == 'Plex Media Server') {
@@ -132,13 +180,15 @@ var getHosts = function () {
                     for (let j = 0; j < connections.length; j++) {
                         if (connections[j].getAttribute('local') == parts[i].getAttribute('publicAddressMatches')) {
                             pmsUrls.set(parts[i].getAttribute('clientIdentifier'), 'http://' + connections[j].getAttribute('address') + ':' + connections[j].getAttribute('port'));
+                            MSG(getJSLocale(Language.Get_the_server_address, { address: 'http://' + connections[j].getAttribute('address') + ':' + connections[j].getAttribute('port') }))
                             break;
                         }
                     }
                 }
             }
         }).catch(function () {
-            showToast('Failed to get PMS URLs', 1);
+            MSG(Language.Failed_to_get_PMS_URLs, 'error')
+            showToast(Language.Failed_to_get_PMS_URLs, 1);
         });
 }
 
@@ -176,7 +226,7 @@ var clickListener = function (e) {
     }
 
     var metaDataPath = pmsUrls.get(serverId) + '/library/metadata/' + mediaId + '?includeConcerts=1&includeExtras=1&includeOnDeck=1&includePopularLeaves=1&includePreferences=1&includeChapters=1&asyncCheckFiles=0&asyncRefreshAnalysis=0&asyncRefreshLocalMediaAgent=0&X-Plex-Token=' + localStorage.myPlexAccessToken;
-    //console.log('metaDataPath = ' + metaDataPath)
+    MSG(getJSLocale(Language.Get_Media_address, { mediaaddress: metaDataPath }), 'debug')
     makeRequest(metaDataPath, serverId)
         .then((response) => {
             let mediaurl = '';
@@ -206,7 +256,7 @@ var clickListener = function (e) {
                         subtitlelist.push(subtitlekey);
                     }
                     else {
-                        logMessage('未找到本地外挂字幕文件', 'Error');
+                        MSG(Language.The_local_external_subtitle_file_was_not_found);
                     }
                 }
             }
@@ -218,8 +268,8 @@ var clickListener = function (e) {
 
             var authedUrl = mediaurl + '&X-Plex-Token=' + token;
             let poturl = "potplayer://" + authedUrl + " /sub=" + subtitleUrl[0] + '?X-Plex-Token=' + token;
-            //console.log(poturl);
-            showToast('成功解析影片 ' + title + ' 路径，正在激活本地PotPlayer播放器。', 0)
+            MSG(poturl, 'debug')
+            showToast(getJSLocale(Language.Successfully_parsed_the_path_of_the_movie, { mediatitle: title }))
             window.open(poturl, "_blank");
         });
 
@@ -315,3 +365,88 @@ jQuery('body').append('<style>.plexextplayericocover {right: 10px; top: 10px; po
 // 放置脚本最后
 setInterval(bindClicks, 100);
 bindClicks();
+
+function debugID() {
+    function S4() {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    }
+    return (S4() + S4());
+}
+
+function getRow() {
+    let err = new Error().stack.split('at')
+    err = err[err.length - 1].split(':')
+    err = err[err.length - 1].replace(')', '')
+    return parseInt(err)
+}
+
+function getJSLocale(key, params) {
+    var result = ""; 	// 对应的资源的内容
+    var paramsObj = {};	// 参数对象
+    if (params) paramsObj = params;
+
+    if (typeof (key) != 'undefined' && typeof (Language) != 'undefined') {
+        // 根据key取得对应的资源内容，如果没有找到则返回key值
+        if (Language[key] != undefined) {
+            result = Language[key];
+        } else {
+            result = key;
+        }
+
+        // 替换对应参数为value的值
+        var regExp = new RegExp(); //替换资源中参数的正则
+        for (var k in paramsObj) {
+            regExp = eval("/{{:" + k + "}}/g");
+            result = result.replace(regExp, paramsObj[k]);
+        }
+
+        // 如果没有找到对应的资源则返回 "No Value"
+        if (/{{:[a-zA-Z]+}}/.test(result)) {
+            result = result.replace(/{{:[a-zA-Z]+}}/g, "No Value");
+        }
+    }
+    return result;
+}
+
+var lang = navigator.language
+var Language = {}
+if (lang === 'zh-CN') {
+    Language = {
+        User_details_not_found: '未找到用户详细信息',
+        Checking_server_list_for_user: '检查第 {{:user}} 个用户的 {{:server}} 服务器列表',
+        Checking_server_with_id: '使用服务器ID {{:id}} 检查服务器',
+        Token_found: '找到令牌:{{:token}}',
+        No_authentication_information_found: '找不到身份验证信息',
+        Verify_permissions_URL: '验证权限URL:{{:authurl}}',
+        Verify_permissions_successfully: '验证权限成功',
+        unauthorized: '未授权',
+        Request_return_status: '请求返回状态:{{:status}}',
+        Call_error_response_code_message: '调用错误:{{:url}}\n响应:{{:responseText}}代码:{{:status}}消息:{{:statusText}}',
+        Find_server_server_address: '查找服务器地址',
+        Get_the_server_address: '得到服务器地址：{{:address}}',
+        Failed_to_get_PMS_URLs: '获取服务器地址失败',
+        Get_Media_address: '得到媒体地址:{{:mediaaddress}}',
+        The_local_external_subtitle_file_was_not_found: '未找到本地外挂字幕文件',
+        Successfully_parsed_the_path_of_the_movie: '成功解析电影{{:mediatitle}}的路径，正在激活本地PotPlayer播放器。'
+    }
+}
+if (lang === 'en') {
+    Language = {
+        User_details_not_found: 'User details not found',
+        Checking_server_list_for_user: 'Checking the {{:server}} service list of the {{:user}}th user',
+        Checking_server_with_id: 'Check the server with server ID {{:id}}',
+        Token_found: 'Token found: {{:token}}',
+        No_authentication_information_found: 'No authentication information found',
+        Verify_permissions_URL: 'Verification authority URL: {{:authurl}}',
+        Verify_permissions_successfully: 'Verify permissions successfully',
+        unauthorized: 'unauthorized',
+        Request_return_status: 'Request return status: {{:status}}',
+        Call_error_response_code_message: 'Call error: {{:url}}\nResponse: {{:responseText}} Code: {{:status}} Message: {{:statusText}}',
+        Find_server_server_address: 'Find server server address',
+        Get_the_server_address: 'Get the server address:{{:address}}',
+        Failed_to_get_PMS_URLs: 'Failed to get PMS URLs',
+        Get_Media_address: 'Get media address:{{:mediaaddress}}',
+        The_local_external_subtitle_file_was_not_found: 'The local external subtitle file was not found',
+        Successfully_parsed_the_path_of_the_movie: 'Successfully parsed the path of the movie {{:mediatitle}}, and the local PotPlayer player is being activated'
+    }
+}
